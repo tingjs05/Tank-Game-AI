b0ba8c5b93a9cd98cbf9d453bd48d6248fb45d9a
Samuel Ting <tingjs05@gmail.com>
2024-09-27 10:35:32 +0900

    nothing of importance was changed

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 python -m venv venv
-venv\Scripts\activate
+venv/Scripts/activate
 python -m pip install --upgrade pip
 python -m pip install mlagents
 python -m pip install torch -f https://download.pytorch.org/whl/torch_stable.html
