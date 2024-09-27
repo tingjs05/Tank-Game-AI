619ddd3c3faf5d62326da44d360be7d830895219
Samuel Ting <tingjs05@gmail.com>
2024-09-27 10:33:33 +0900

    idk what im doing with batch files ._.

---
+++ setup.bat
@@ -0,0 +1,5 @@
+python -m venv venv
+venv\Scripts\activate
+python -m pip install --upgrade pip
+python -m pip install mlagents
+python -m pip install torch -f https://download.pytorch.org/whl/torch_stable.html
