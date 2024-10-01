66d3a12ada207259583f982a4c880dc358f202a8
Samuel Ting <tingjs05@gmail.com>
2024-10-01 14:05:55 +0900

    updated setup bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 python -m venv venv
 venv/Scripts/activate
 python -m pip install --upgrade pip
+pip3 install torch~=2.2.1 --index-url https://download.pytorch.org/whl/cu121
+pip3 install grpcio
 python -m pip install mlagents
-python -m pip install torch -f https://download.pytorch.org/whl/torch_stable.html
