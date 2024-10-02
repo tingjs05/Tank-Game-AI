2550367abf82b427350a7cc5fe0cccea53cffb19
Samuel Ting <tingjs05@gmail.com>
2024-10-02 15:51:12 +0900

    wrote some stuff

--- Assets/Scripts/AI/FSM/TankFSM.cs
+++ Assets/Scripts/AI/FSM/TankFSM.cs
@@ -8,10 +8,15 @@ namespace AI.FSM
     [RequireComponent(typeof(TankController), typeof(ObstacleDetectionManager))]
     public class TankFSM : StateMachine<TankFSM>
     {
+        [SerializeField] Transform target;
+        TankController controller;
+        ObstacleDetectionManager obstacleDetection;
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            controller = GetComponent<TankController>();
+            obstacleDetection = GetComponent<ObstacleDetectionManager>();
         }
     }
 }
