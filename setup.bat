1365218a36d4082a3cf725f50d0488263c83b4cb
Samuel Ting <tingjs05@gmail.com>
2024-10-10 11:32:48 +0900

    added six install to bat file

--- setup.bat
+++ setup.bat
@@ -3,4 +3,5 @@ venv/Scripts/activate
 python -m pip install --upgrade pip
 pip3 install torch~=2.2.1 --index-url https://download.pytorch.org/whl/cu121
 pip3 install grpcio
+pip3 install six
 python -m pip install mlagents
