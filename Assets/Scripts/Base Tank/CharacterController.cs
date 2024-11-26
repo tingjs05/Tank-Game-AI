e96de2fe3fe04edada1a8a70189b55f001b3e45a
Samuel Ting <tingjs05@gmail.com>
2024-11-26 13:05:03 +0900

    fixed character controller issue

--- Assets/Scripts/Base Tank/CharacterController.cs
+++ Assets/Scripts/Base Tank/CharacterController.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.MLAgents.Integrations.Match3;
 using UnityEngine;
 
 [RequireComponent(typeof(TankController))]
@@ -30,23 +31,27 @@ public class CharacterController : MonoBehaviour
         // set tank
         if (tankBarrel != null && tankChassis != null)
         {
+            // apply barrel material
             tankBarrel.material = characters[index].bodyMaterial;
-            tankChassis.materials[1] = characters[index].bodyMaterial;
-            tankChassis.materials[2] = characters[index].trackMaterial;
+            // apply other chassis materials
+            Material[] chassisMats = tankChassis.materials;
+            chassisMats[0] = characters[index].bodyMaterial;
+            chassisMats[1] = characters[index].trackMaterial;
+            tankChassis.materials = chassisMats;
         }
 
         // set character model
         for (int i = 0; i < characters.Length; i++)
         {
+            // hide characters that do not match the index
             if (i != index)
             {
                 characters[i].characterAnim.gameObject.SetActive(false);
                 continue;
             }
-
+            // show character and set animator to controller
             characters[i].characterAnim.gameObject.SetActive(true);
             controller.characterAnimator = characters[i].characterAnim;
-
             // reset character position
             characters[i].characterAnim.gameObject.transform.localPosition = 
                 new Vector3(characters[i].characterAnim.gameObject.transform.localPosition.x, 0f, 
